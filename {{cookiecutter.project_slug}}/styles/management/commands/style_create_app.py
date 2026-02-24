b1afb15e4911971a649f5794316f84064535bee7
jafar <jafar0650developer@gmail.com>
2026-02-24 13:14:51 +0330

    🐳 chore(docker): update Python version in Dockerfile to 3.12 and add necessary build dependencies
    📦 refactor(style_create_app): streamline app directory creation and file initialization in style_create_app command

--- {{cookiecutter.project_slug}}/styles/management/commands/style_create_app.py
+++ {{cookiecutter.project_slug}}/styles/management/commands/style_create_app.py
@@ -1,5 +1,4 @@
 import os
-
 from django.conf import settings
 from django.core.management.base import BaseCommand
 
@@ -8,53 +7,63 @@ BASE_DIR = settings.BASE_DIR.__str__()
 
 class Command(BaseCommand):
     def create_app(self, app_name):
-        os.system(f"mkdir {BASE_DIR}/{app_name}")
-        os.system(f"touch {BASE_DIR}/{app_name}/__init__.py")
+        app_path = os.path.join(BASE_DIR, app_name)
 
-        os.system(f"mkdir {BASE_DIR}/{app_name}/migrations")
-        os.system(f"touch {BASE_DIR}/{app_name}/migrations/__init__.py")
+        # ایجاد پوشه اصلی و زیرپوشه‌ها
+        folders = [
+            app_path,
+            os.path.join(app_path, "migrations"),
+            os.path.join(app_path, "tests"),
+            os.path.join(app_path, "api"),
+        ]
 
-        os.system(f"mkdir {BASE_DIR}/{app_name}/tests")
-        os.system(f"touch {BASE_DIR}/{app_name}/tests/__init__.py")
+        for folder in folders:
+            if not os.path.exists(folder):
+                os.makedirs(folder)
 
-        os.system(f"mkdir {BASE_DIR}/{app_name}/api")
-        os.system(f"touch {BASE_DIR}/{app_name}/api/__init__.py")
+        # تابع کمکی برای ایجاد فایل و نوشتن محتوا
+        def create_file(filename, content):
+            with open(filename, "w", encoding="utf-8") as f:
+                f.write(content)
 
-        os.system(f"touch {BASE_DIR}/{app_name}/api/serializers.py")
-        serializers_text = f"from rest_framework import serializers\n\n# Create your serializers here."
-        os.system(f"echo '{serializers_text}' > {BASE_DIR}/{app_name}/api/serializers.py")
+        # ایجاد فایل‌های __init__.py
+        init_files = [
+            os.path.join(app_path, "__init__.py"),
+            os.path.join(app_path, "migrations", "__init__.py"),
+            os.path.join(app_path, "tests", "__init__.py"),
+            os.path.join(app_path, "api", "__init__.py"),
+        ]
+        for file in init_files:
+            create_file(file, "")
 
-        os.system(f"touch {BASE_DIR}/{app_name}/api/api_views.py")
-        api_views_text = f"from rest_framework import generics\n\n# Create your views here."
-        os.system(f"echo '{api_views_text}' > {BASE_DIR}/{app_name}/api/api_views.py")
+        # ایجاد فایل‌های api
+        serializers_text = "from rest_framework import serializers\n\n# Create your serializers here."
+        create_file(os.path.join(app_path, "api", "serializers.py"), serializers_text)
 
-        os.system(f"touch {BASE_DIR}/{app_name}/api/urls.py")
-        api_urls_text = f"from django.urls import path\n\nurlpatterns = [\n\n]"
-        os.system(f"echo '{api_urls_text}' > {BASE_DIR}/{app_name}/api/urls.py")
+        api_views_text = "from rest_framework import generics\n\n# Create your views here."
+        create_file(os.path.join(app_path, "api", "api_views.py"), api_views_text)
 
-        os.system(f"touch {BASE_DIR}/{app_name}/admin.py")
-        admin_text = f"from django.contrib import admin\n\n# Register your models here."
-        os.system(f"echo '{admin_text}' > {BASE_DIR}/{app_name}/admin.py")
+        api_urls_text = "from django.urls import path\n\nurlpatterns = [\n\n]"
+        create_file(os.path.join(app_path, "api", "urls.py"), api_urls_text)
 
-        os.system(f"touch {BASE_DIR}/{app_name}/apps.py")
-        apps_text = f"from django.apps import AppConfig\n\n\nclass {app_name.capitalize()}Config(AppConfig):\n    default_auto_field = \"django.db.models.BigAutoField\"\n    name = \"{app_name}\" \n    import {app_name}.signals"
-        os.system(f"echo '{apps_text}' > {BASE_DIR}/{app_name}/apps.py")
+        # ایجاد فایل‌های روت
+        admin_text = "from django.contrib import admin\n\n# Register your models here."
+        create_file(os.path.join(app_path, "admin.py"), admin_text)
 
-        os.system(f"touch {BASE_DIR}/{app_name}/models.py")
-        models_text = f"from django.db import models\n\n# Create your models here."
-        os.system(f"echo '{models_text}' > {BASE_DIR}/{app_name}/models.py")
+        apps_text = f"""from django.apps import AppConfig\n\n\nclass {app_name.capitalize()}Config(AppConfig):\n    default_auto_field = "django.db.models.BigAutoField"\n    name = "{app_name}" \n    import {app_name}.signals"""
+        create_file(os.path.join(app_path, "apps.py"), apps_text)
 
-        os.system(f"touch {BASE_DIR}/{app_name}/signals.py")
-        signals_text = f"from django.db.models.signals import post_save\nfrom django.dispatch import receiver\n\n\n# Create your signals here."
-        os.system(f"echo '{signals_text}' > {BASE_DIR}/{app_name}/signals.py")
+        models_text = "from django.db import models\n\n# Create your models here."
+        create_file(os.path.join(app_path, "models.py"), models_text)
 
-        os.system(f"touch {BASE_DIR}/{app_name}/exceptions.py")
+        signals_text = "from django.db.models.signals import post_save\nfrom django.dispatch import receiver\n\n\n# Create your signals here."
+        create_file(os.path.join(app_path, "signals.py"), signals_text)
+
         exceptions_text = f"from rest_framework.exceptions import APIException\n\n\nclass {app_name.capitalize()}Exception(APIException):\n    pass"
-        os.system(f"echo '{exceptions_text}' > {BASE_DIR}/{app_name}/exceptions.py")
+        create_file(os.path.join(app_path, "exceptions.py"), exceptions_text)
 
-        os.system(f"touch {BASE_DIR}/{app_name}/permissions.py")
         permissions_text = f"from rest_framework.permissions import BasePermission\n\n\nclass {app_name.capitalize()}Permission(BasePermission):\n    pass"
-        os.system(f"echo '{permissions_text}' > {BASE_DIR}/{app_name}/permissions.py")
+        create_file(os.path.join(app_path, "permissions.py"), permissions_text)
 
     def add_arguments(self, parser):
         parser.add_argument('app_name', type=str)
